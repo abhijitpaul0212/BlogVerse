c1fd7cda4a7e8317f8f5ea654373019646b4de78
Abhijit Paul <abhijitpaul0212@gmail.com>
2023-09-24 14:20:48 +0530

    Update application.py

--- application.py
+++ application.py
@@ -13,36 +13,60 @@ from project.routes.blogs.routes import get_all_categories
 if os.environ.get("FDT") == "ON":
     from flask_debugtoolbar import DebugToolbarExtension
 
+application=Flask(__name__, static_folder="project//static", template_folder="project//templates")
+app=application
 
-# --- // Application Factory Setup (based on the Flask-User example for MongoDB)
-# https://flask-user.readthedocs.io/en/latest/mongodb_app.html
-def create_app():    
-    # Setup Flask and load app.config
-    application = app = Flask(__name__, static_folder="project//static", template_folder="project//templates")
-    app.config.from_object(__name__ + ".DevelopmentConfig")
-    # csrf = CSRFProtect(app)
-    # csrf.init_app(app)
-
-    if os.environ.get("FDT") == "ON":
+if os.environ.get("FDT") == "ON":
         app.debug = True
 
-    # Setup Flask-MongoEngine --> MongoEngine --> PyMongo --> MongoDB
-    db.init_app(app)
+# Setup Flask-MongoEngine --> MongoEngine --> PyMongo --> MongoDB
+db.init_app(app)
 
-    UserManager(app, db, User)
+UserManager(app, db, User)
 
-    # Use Flask Sessions with Mongoengine
-    app.session_interface = MongoEngineSessionInterface(db)
+# Use Flask Sessions with Mongoengine
+app.session_interface = MongoEngineSessionInterface(db)
 
-    # Initiate the Flask Debug Toolbar Extension
-    if os.environ.get("FDT") == "ON":
-        toolbar = DebugToolbarExtension(app)
+# Initiate the Flask Debug Toolbar Extension
+if os.environ.get("FDT") == "ON":
+    toolbar = DebugToolbarExtension(app)
 
-    # Register blueprints here
-    app.register_blueprint(blogs_bp)
-    app.register_blueprint(main_bp)
+# Register blueprints here
+app.register_blueprint(blogs_bp)
+app.register_blueprint(main_bp)
 
-    with app.app_context():
-        get_all_categories()
+with app.app_context():
+    get_all_categories()
+        
+# --- // Application Factory Setup (based on the Flask-User example for MongoDB)
+# https://flask-user.readthedocs.io/en/latest/mongodb_app.html
+# def create_app():    
+#     # Setup Flask and load app.config
+#     application = app = Flask(__name__, static_folder="project//static", template_folder="project//templates")
+#     app.config.from_object(__name__ + ".DevelopmentConfig")
+#     # csrf = CSRFProtect(app)
+#     # csrf.init_app(app)
 
-    return app
+#     if os.environ.get("FDT") == "ON":
+#         app.debug = True
+
+#     # Setup Flask-MongoEngine --> MongoEngine --> PyMongo --> MongoDB
+#     db.init_app(app)
+
+#     UserManager(app, db, User)
+
+#     # Use Flask Sessions with Mongoengine
+#     app.session_interface = MongoEngineSessionInterface(db)
+
+#     # Initiate the Flask Debug Toolbar Extension
+#     if os.environ.get("FDT") == "ON":
+#         toolbar = DebugToolbarExtension(app)
+
+#     # Register blueprints here
+#     app.register_blueprint(blogs_bp)
+#     app.register_blueprint(main_bp)
+
+#     with app.app_context():
+#         get_all_categories()
+
+#     return app
